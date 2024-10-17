72ea131f1aa8cfcabcde87c6b85e326a575f656e
Aryan Verma <aryan.computerscience@gmail.com>
2024-10-17 18:01:51 +0530

    complete

--- models/author.js
+++ models/author.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const { DateTime } = require("luxon");
 const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
@@ -9,17 +9,36 @@ const AuthorSchema = new Schema({
   date_of_death: { type: Date },
 });
 
-AuthorSchema.virtual("name").get(() => {
-  let fullname = "";
-  if(this.first_name && this.family_name) {
-    fullname = `${this.family_name, this.first_name}`
-  }
-
-  return fullname;
+AuthorSchema.virtual("name").get(function () {
+  return this.family_name + ", " + this.first_name;
 })
 
-AuthorSchema.virtual("url").get(() => {
+AuthorSchema.virtual("url").get(function () {
   return `/catalog/author/${this._id}`;
 })
+
+AuthorSchema.virtual("lifespan").get(function () {
+  let lifetime_string = "";
+  if (this.date_of_birth) {
+    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+  lifetime_string += " - ";
+  if (this.date_of_death) {
+    lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+  return lifetime_string;
+});
+
+AuthorSchema.virtual("date_of_birth_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toISODate(); // format 'YYYY-MM-DD'
+});
+
+AuthorSchema.virtual("date_of_death_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toISODate(); // format 'YYYY-MM-DD'
+});
 
 module.exports = mongoose.model("Author", AuthorSchema);
